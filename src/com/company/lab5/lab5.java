ec639c495be5c50969053ea7b602634dd016ac66
Ярослав <mulo-_-2@mail.ru>
2018-04-03 22:07:56 +0300

    Lab5- bug fixed. Added common exercise

--- src/com/company/lab5/lab5.java
+++ src/com/company/lab5/lab5.java
@@ -1,28 +1,31 @@
 package com.company.lab5;
 
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
 
 public class lab5 {
     public static void main(String[] args) {
+        System.out.println("OK");
+        OneLinkedList oneLinkedList=new OneLinkedList(10);
+
         List<Integer> list= new ArrayList<>();
-        for(int i=10; i>0;i--)
-        {
-            list.add(i);
-        }
-        System.out.println(list);
-        RemoveLast removeLast=new RemoveLast();
-        System.out.println(removeLast.removeLastEl(list));
-        InvertList invertList=new InvertList();
-        System.out.println(invertList.ivertList(list));
-        SortList sortList=new SortList();
-        System.out.println(sortList.sortList(list));
-        GetLength length=new GetLength();
-        System.out.println(length.size(list));
+//        for(int i=10; i>0;i--)
+//        {
+//            oneLinkedList.addLast(i);
+//        }
+        System.out.println(oneLinkedList);
+        oneLinkedList.invert();
+        System.out.println(oneLinkedList);
+        oneLinkedList.removeLast();
+        System.out.println(oneLinkedList);
+        oneLinkedList.sort();
+        System.out.println(oneLinkedList);
+       /* System.out.println(list);
         Vector v1= new Vector();
         Vector v2= new Vector();
         v1.setCoordinatesVector(3,4);
         v2.setCoordinatesVector(4,3);
-        System.out.println(v1.getAngleBetweenVector(v2));
+        System.out.println(v1.getAngleBetweenVector(v2));*/
     }
 }
