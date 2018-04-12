99e8d5e72bd6beb4dfb6e3e75ee82dd298c14305
Ярослав <mulo-_-2@mail.ru>
2018-04-12 04:05:40 +0300

    lab8- added individual ex

---
+++ src/com/company/lab8/ListString.java
@@ -0,0 +1,34 @@
+package com.company.lab8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListString {
+    List<String> findPolindrom(List<String> stringList)
+    {
+        List<String> resultList=new ArrayList<>();
+        boolean isPolindrom=false;
+        for(String currentStr :stringList)
+        {
+            char [] arrCh= currentStr.toCharArray();
+            int mid=currentStr.length()/2;
+            for(int i=0, j=currentStr.length()-1;i<mid&&j>mid;i++, j--)
+            {
+                if(arrCh[i]==arrCh[j])
+                {
+                    isPolindrom= true;
+                }
+                else
+                {
+                    isPolindrom=false;
+                    break;
+                }
+            }
+            if(isPolindrom)
+            {
+                resultList.add(currentStr);
+            }
+        }
+        return resultList;
+    }
+}
